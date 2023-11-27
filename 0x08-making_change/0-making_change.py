4a0c50a11b65129d5351abab339e3d0d809136fb
NewGit1999 <yasminebenali32@gmail.com>
2023-11-27 15:47:16 -0800

    fewest nb of coin

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""fowest number of coins"""
+
+
+def makeChange(coins, total):
+    """determine the fewest number of coins needed"""
+    if total < 0:
+        return 0
+    if total == 0:
+        return 0
+
+    nb = [float('inf')] * (total + 1)
+    nb[0] = 0
+
+    for i in range(1, total + 1):
+        for coin in coins:
+            if i - coin >= 0:
+                nb[i] = min(nb[i], dp[i - coin] + 1)
+
+    if nb[total] == float('inf'):
+        return -1
+    else:
+        return nb[total]
