47ede0ff029550f1a511e3338aa541a4aaf864ae
NewGit1999 <yasminebenali32@gmail.com>
2024-01-26 05:26:11 -0800

    fix error

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,91 +1,69 @@
 #!/usr/bin/python3
-'''N Queens Challenge'''
-
+"""N-Queens challenge with backtracking"""
 import sys
 
 
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
+def chessboard(pos, n):
+    """Function to print chessboard with appropriate positions of queens"""
 
-    try:
-        n = int(sys.argv[1])
-    except ValueError:
-        print('N must be a number')
-        exit(1)
+    board = []
 
-    if n < 4:
-        print('N must be at least 4')
-        exit(1)
+    for RANK in range(n):
+        for FILE in range(n):
+            if FILE == pos[RANK]:
+                board.append([RANK, FILE])
 
-    solutions = []
-    placed_queens = []  # coordinates format [row, column]
-    stop = False
-    r = 0
-    c = 0
+    print(board)
 
-    # iterate thru rows
-    while r < n:
-        goback = False
-        # iterate thru columns
-        while c < n:
-            # check is current column is safe
+
+def safe_pos(pos, RANK, FILE, n):
+    """Function to determine safe a square to place a queen"""
+
+    if (pos[RANK] == FILE) or (pos[RANK] == FILE - RANK + n) or \
+            (pos[RANK] == RANK - n + FILE):
+        return True
+    return False
+
+
+def get_safe_pos(board, rank, n):
+    """Function to get all safe positions to place queens with recursion"""
+
+    if rank == n:
+        chessboard(board, n)
+    else:
+        for FILE in range(n):
             safe = True
-            for cord in placed_queens:
-                col = cord[1]
-                if(col == c or col + (r-cord[0]) == c or
-                        col - (r-cord[0]) == c):
+
+            for RANK in range(rank):
+                if safe_pos(board, RANK, FILE, rank):
                     safe = False
-                    break
 
-            if not safe:
-                if c == n - 1:
-                    goback = True
-                    break
-                c += 1
-                continue
+            if safe:
+                board[rank] = FILE
+                get_safe_pos(board, rank + 1, n)
 
-            # place queen
-            cords = [r, c]
-            placed_queens.append(cords)
-            # if last row, append solution and reset all to last unfinished row
-            # and last safe column in that row
-            if r == n - 1:
-                solutions.append(placed_queens[:])
-                for cord in placed_queens:
-                    if cord[1] < n - 1:
-                        r = cord[0]
-                        c = cord[1]
-                for i in range(n - r):
-                    placed_queens.pop()
-                if r == n - 1 and c == n - 1:
-                    placed_queens = []
-                    stop = True
-                r -= 1
-                c += 1
-            else:
-                c = 0
-            break
-        if stop:
-            break
-        # on fail: go back to previous row
-        # and continue from last safe column + 1
-        if goback:
-            r -= 1
-            while r >= 0:
-                c = placed_queens[r][1] + 1
-                del placed_queens[r]  # delete previous queen coordinates
-                if c < n:
-                    break
-                r -= 1
-            if r < 0:
-                break
-            continue
-        r += 1
 
-    for idx, val in enumerate(solutions):
-        if idx == len(solutions) - 1:
-            print(val, end='')
-        else:
-            print(val)
+def create_board(n):
+    """Function to generate chessboard of n-size"""
+
+    return [0 * n for i in range(n)]
+
+
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    exit(1)
+
+try:
+    n = int(sys.argv[1])
+except Exception:
+    print("N must be a number")
+    exit(1)
+
+if (n < 4):
+    print("N must be at least 4")
+    exit(1)
+
+
+board = create_board(int(n))
+rank = 0
+get_safe_pos(board, rank, int(n))
